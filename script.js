f5b8f25ed463471958123ab5bbe90fa42b90ed4a
Aidar Nigmatullin <gudwinthegreat@gmail.com>
2020-11-11 19:49:16 +0300

    added css

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-//написано Нигматуллиным Айдаром Азатовичем. по вопросам звоните 89655923382
+//написано Нигматуллиным Айдаром Азатовичем. по вопросам звоните указано в анкете
 
 function sayHiIfValueUpper7 (event) {
 	const value = event.target.value;
 	const rezult = (Number.isSafeInteger(+value) && value >7)? "Привет" : "";
 	const objectiveName = UI.getObjectiveName(event.target.className);
-	UI.setAnswer(objectiveName, rezult)
+	UI.setAnswer(objectiveName, rezult);
 }
 
 function sayHiToVitek (event){
 	const value = event.target.value;
-	const rezult = value === "Вячеслав"? "Привет, Вячеслав" : "Нет такого имени";
+	const rezult = (value === "Вячеслав")? "Привет, Вячеслав" : "Нет такого имени";
 	const objectiveName = UI.getObjectiveName(event.target.className);
-	UI.setAnswer(objectiveName, rezult)
+	UI.setAnswer(objectiveName, rezult);
 }
 
 function multipleOfThree (event){
 	const value = event.target.value;
-	const array = value.trim().split(",")
+	const array = value.trim().split(",");
 
 	let rezultArray = array.filter((number)=>{
 		const typecastingOfVariable = +number;
@@ -26,7 +26,7 @@ function multipleOfThree (event){
 		else {
 			return false
 		}
-	})
+	});
 	const objectiveName = UI.getObjectiveName(event.target.className);
 	UI.setAnswer(objectiveName, rezultArray.toString());
 }
@@ -71,46 +71,18 @@ class ui {
 		this.setAnswer = (objectiveName, value) => {
 			document.getElementsByClassName("p_"+objectiveName)[0].innerText = value;
 		}
-		
 	}
 }
 
 let UI = new ui;
-
 const elemNameArray = ["sayHiIfValueUpper7","sayHiToVitek","multipleOfThree"];
 elemNameArray.map(element=>{
 	let objective = document.createElement("div");
-	let header = objective.appendChild(document.createElement("h2"))
+	objective.className = element;
+	let header = objective.appendChild(document.createElement("h2"));
 	header.innerHTML = UI.getTitle(element);
 	let input = objective.appendChild(UI.setInput(element));
 	input.setAttribute("onchange", element+'(event)');
 	objective.appendChild(UI.setP(element));
-	document.body.insertBefore(objective, document.getElementById("main"))
-})
-
-/*
-let objective1 = document.createElement("div");
-let header1 = objective1.appendChild(document.createElement("h2"))
-header1.innerHTML = "Введите число"
-let input1 = objective1.appendChild(UI.setInput("sayHiIfValueUpper7"));
-input1.setAttribute("onchange", 'sayHiIfValueUpper7(event)');
-let p1 = objective1.appendChild(UI.setP("sayHiIfValueUpper7"));
-
-let objective2 = document.createElement("div");
-let header2 = objective2.appendChild(document.createElement("h2"))
-header2.innerHTML = "Введите имя";
-let input2 = objective2.appendChild(UI.setInput("sayHiToVitek"));
-input2.setAttribute("onchange", 'sayHiToVitek(event)');
-let p2 = objective2.appendChild(UI.setP("sayHiToVitek"));
-
-let objective3 = document.createElement("div");
-let header3 = objective3.appendChild(document.createElement("h2"))
-header3.innerHTML = "Введите числа через запятую"
-let input3 = objective3.appendChild(UI.setInput("multipleOfThree"));
-input3.setAttribute("onchange", 'multipleOfThree(event)');
-let p3 = objective3.appendChild(UI.setP("multipleOfThree"));
-
-// document.body.insertBefore(objective1, document.getElementById("main"));
-document.body.insertBefore(objective2, document.getElementById("main"));
-document.body.insertBefore(objective3, document.getElementById("main"));
-*/
+	document.body.insertBefore(objective, document.getElementById("main"));
+});
