080e7241ff88869c5061cd0d289f81aa53731d57
Aidar Nigmatullin <gudwinthegreat@gmail.com>
2020-11-12 10:59:37 +0300

    changed file name to index.html added class

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 
 function sayHiIfValueUpper7 (event) {
 	const value = event.target.value;
-	const rezult = (Number.isSafeInteger(+value) && value >7)? "Привет" : "";
+	const rezult = functionalParadigm.sayHiIfValueUpper7(value);
 	const objectiveName = UI.getObjectiveName(event.target.className);
 	UI.setAnswer(objectiveName, rezult);
 }
 
 function sayHiToVitek (event){
 	const value = event.target.value;
-	const rezult = (value === "Вячеслав")? "Привет, Вячеслав" : "Нет такого имени";
+	const rezult = functionalParadigm.sayHiToVitek(value)
 	const objectiveName = UI.getObjectiveName(event.target.className);
 	UI.setAnswer(objectiveName, rezult);
 }
@@ -18,30 +18,15 @@ function multipleOfThree (event){
 	const value = event.target.value;
 	const array = value.trim().split(",");
 
-	let rezultArray = array.filter((number)=>{
-		const typecastingOfVariable = +number;
-		if (Number.isSafeInteger(typecastingOfVariable) && typecastingOfVariable !=0){
-			return (typecastingOfVariable%3==0);
-		}
-		else {
-			return false
-		}
-	});
+	let rezultArray = functionalParadigm.multipleOfThree(array);
 	const objectiveName = UI.getObjectiveName(event.target.className);
 	UI.setAnswer(objectiveName, rezultArray.toString());
 }
 
 class ui {
 	constructor(){
-		this.setInput = (objectiveName) => {
-			let input = document.createElement("input");
-			input.className = "input_"+objectiveName;
-			return input;
-		}
-		this.setP = (objectiveName) => {
-			let p = document.createElement("p");
-			p.className = "p_"+objectiveName;
-			return p
+		this.setAnswer = (objectiveName, value) => {
+			document.getElementsByClassName("p_"+objectiveName)[0].innerText = value;
 		}
 		this.getTitle = (objectiveName)=>{
 			let rezult = "";
@@ -61,13 +46,43 @@ class ui {
 		this.getObjectiveName = (objectiveNameWith_) => {
 			return objectiveNameWith_.split("_")[1];
 		}
-		this.setAnswer = (objectiveName, value) => {
-			document.getElementsByClassName("p_"+objectiveName)[0].innerText = value;
+		this.setInput = (objectiveName) => {
+			let input = document.createElement("input");
+			input.className = "input_"+objectiveName;
+			return input;
+		}
+		this.setP = (objectiveName) => {
+			let p = document.createElement("p");
+			p.className = "p_"+objectiveName;
+			return p
+		}
+	}
+}
+
+class inFunctionalParadigm {
+	constructor(){
+		this.sayHiIfValueUpper7 = (value)=> {
+			return (Number.isSafeInteger(+value) && value >7)? "Привет" : "";
+		}
+		this.sayHiToVitek = (value)=>{
+			return (value === "Вячеслав")? "Привет, Вячеслав" : "Нет такого имени";
+		}
+		this.multipleOfThree = (value)=>{
+			return value.filter((number)=>{
+				const typecastingOfVariable = +number;
+				if (Number.isSafeInteger(typecastingOfVariable) && typecastingOfVariable !=0){
+					return (typecastingOfVariable%3==0);
+				}
+				else {
+					return false
+				}
+			});
 		}
 	}
 }
 
 let UI = new ui;
+const functionalParadigm = new inFunctionalParadigm;
 const elemNameArray = ["sayHiIfValueUpper7","sayHiToVitek","multipleOfThree"];
 elemNameArray.map(element=>{
 	let objective = document.createElement("div");
@@ -75,6 +90,7 @@ elemNameArray.map(element=>{
 	let header = objective.appendChild(document.createElement("h2"));
 	header.innerHTML = UI.getTitle(element);
 	let input = objective.appendChild(UI.setInput(element));
+	// event Handler
 	input.setAttribute("onchange", element+'(event)');
 	objective.appendChild(UI.setP(element));
 	document.body.insertBefore(objective, document.getElementById("main"));
